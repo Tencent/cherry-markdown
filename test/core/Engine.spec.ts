c2e0c56acdc2b19a52b623d6ba9fe352013063b4
jiawei686 <892001108@qq.com>
2022-02-14 18:34:57 +0800

    feat: add commonmark test suites

--- test/core/Engine.spec.ts
+++ test/core/Engine.spec.ts
@@ -1,25 +1,44 @@
 import fs from 'fs'
 import util from 'util';
 import path from 'path';
+import { diff } from 'jest-diff';
 import Cherry from '../../src/index';
 import suites from '../suites/commonmark.spec.json';
 
 const TEST_PATH = '../suites/engine.md';
 const readFile = (fileName) => util.promisify(fs.readFile)(fileName, 'utf8');
-const cherry = new Cherry({});
+const cherry = new Cherry({
+  engine: {
+    global: {
+      classicBr: true
+    }
+  }
+});
+
+expect.extend({
+  matchHTML(result, standard) {
+    const resultWithoutAttr = result.replace(/\s+(data-lines=".+?")(\s+data-type=".+?")?\s+data-sign=".+?"/gm, '');
+    const pass = standard === resultWithoutAttr;
+    const message = pass ? '' : diff(standard, resultWithoutAttr);
+    return {
+      pass,
+      message: () => message,
+    };
+  },
+});
 
 describe('engine', () => {
-  test('engine.md', () => {
-    return readFile(path.join(__dirname, TEST_PATH)).then((data) => {
-      // need to ignore error while accessing engine(private)
-      // @ts-ignore
-      expect(cherry.engine.makeHtml(data)).toMatchSnapshot();
-    });
-  });
-  test('commonmark', () => {
-    suites.forEach((item) => {
-      // @ts-ignore
-      expect(cherry.engine.makeHtml(item.markdown)).toBe(item.html);
+  // test('engine.md', () => {
+  //   return readFile(path.join(__dirname, TEST_PATH)).then((data) => {
+  //     // need to ignore error while accessing engine(private)
+  //     // @ts-ignore
+  //     expect(cherry.engine.makeHtml(data)).toMatchSnapshot();
+  //   });
+  // });
+  suites.forEach((item, index) => {
+    test(`commonmark-${index}`, () => {
+    // @ts-ignore
+      expect(cherry.engine.makeHtml(item.markdown)).matchHTML(item.html);
     });
   });
 });
