67766e2fcc613c3871a63aabb908cc4990f94602
jiawei686 <892001108@qq.com>
2022-04-02 10:52:53 +0800

    feat: update unit test

--- jest.config.ts
+++ jest.config.ts
@@ -85,7 +85,6 @@ export default {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper:{
     '@/(.*)$': '<rootDir>/src/$1',
-    '\\.(css|less|scss)$': '<rootDir>/test/styleMock.js',
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
