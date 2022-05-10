8d72b19d3f4b224274fb2d8c2a757ed8fcf06067
Jared Wang <stillba@icloud.com>
2022-05-10 10:23:34 +0800

    fix(code-block): fix #166 parse hr and indent code (#205)
    * fix(code-block): fix #166 parse hr and indent code
    
    * fix(code-block): remove lookbehind assertion and prepend line feed
    
    Co-authored-by: jaredjywang <jaredjywang@tencent.com>

--- src/core/hooks/CodeBlock.js
+++ src/core/hooks/CodeBlock.js
@@ -17,6 +17,7 @@ import ParagraphBase from '@/core/ParagraphBase';
 import Prism from 'prismjs';
 import { escapeHTMLSpecialChar } from '@/utils/sanitize';
 import { getCodeBlockRule } from '@/utils/regexp';
+import { prependLineFeedForParagraph } from '@/utils/lineFeed';
 
 Prism.manual = true;
 
@@ -203,7 +204,7 @@ export default class CodeBlock extends ParagraphBase {
    */
   $getIndentedCodeReg() {
     const ret = {
-      begin: '(?:^|\\n\\s*\\n)(?: {4}|\\t)',
+      begin: '(?:^|\\n)(?: {4}|\\t)',
       end: '(?=$|\\n( {0,3}[^ \\t\\n]|\\n[^ \\t\\n]))',
       content: '([\\s\\S]+?)',
     };
@@ -218,13 +219,13 @@ export default class CodeBlock extends ParagraphBase {
       return str;
     }
     return this.$recoverCodeInIndent(str).replace(this.$getIndentedCodeReg(), (match, code) => {
-      const lineCount = (match.match(/\n/g) || []).length - 1;
+      const lineCount = (match.match(/\n/g) || []).length;
       const sign = this.$engine.md5(match);
       const html = `<pre data-sign="${sign}" data-lines="${lineCount}"><code>${escapeHTMLSpecialChar(
         code.replace(/\n( {4}|\t)/g, '\n'),
       )}</code></pre>`;
       // return this.getCacheWithSpace(this.pushCache(html), match, true);
-      return this.pushCache(html, sign, lineCount);
+      return prependLineFeedForParagraph(match, this.pushCache(html, sign, lineCount));
     });
   }
 
