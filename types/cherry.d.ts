3522a1dfcf9ed92956e44552b6b509b3bf882975
sunsonliu <sunsonliu@tencent.com>
2022-06-09 21:55:40 +0800

    feat: 暴露预览区域图片点击事件的回调

--- types/cherry.d.ts
+++ types/cherry.d.ts
@@ -22,6 +22,7 @@ export interface CherryOptions {
     afterInit: CherryLifecycle;
     /** img 标签挂载前触发，可用于懒加载等场景 */
     beforeImageMounted: (srcProp: string, src: string) => { srcProp: string; src: string };
+    onClickPreview: (e: MouseEvent) => void;
   };
   /** 预览区域配置 */
   previewer: CherryPreviewerOptions;
