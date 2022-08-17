da8cc9d60f50868edda9db4f814d08a75fc4571f
YuanYe <962536884@qq.com>
2022-08-17 10:43:09 +0800

    fix:修复了codeblock的缩进，增加了通过的测试用例

--- src/core/hooks/CodeBlock.js
+++ src/core/hooks/CodeBlock.js
@@ -206,7 +206,7 @@ export default class CodeBlock extends ParagraphBase {
    */
   $getIndentedCodeReg() {
     const ret = {
-      begin: '(?:^|\\n\\s*\\n)(?: {4}|\\t)',
+      begin: '(?:^|\\n\\s*\\n)(?: {4}|\\t| {1:4}\\t)',
       end: '(?=$|\\n( {0,3}[^ \\t\\n]|\\n[^ \\t\\n]))',
       content: '([\\s\\S]+?)',
     };
@@ -225,7 +225,7 @@ export default class CodeBlock extends ParagraphBase {
       const sign = this.$engine.md5(match);
       const html = `<pre data-sign="${sign}" data-lines="${lineCount}"><code>${escapeHTMLSpecialChar(
         code.replace(/\n( {4}|\t)/g, '\n'),
-      )}</code></pre>`;
+      )}</code></pre>\n`;
       // return this.getCacheWithSpace(this.pushCache(html), match, true);
       return prependLineFeedForParagraph(match, this.pushCache(html, sign, lineCount));
     });
