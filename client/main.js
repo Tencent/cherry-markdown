b819a11703e3bc80e8e606afee9c7ddbdaa2ba8c
Kryptonite <1535820963@qq.com>
2023-02-25 21:27:17 +0800

    fix: client窗体关闭提示

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { dialog, app, BrowserWindow } = require('electron')
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -7,16 +7,36 @@ function createWindow() {
     })
 
     win.loadFile('index.html');
+    return win;
     // win.webContents.openDevTools();
 }
 
 app.whenReady().then(() => {
-    createWindow()
+    mainWindow = createWindow()
     app.on('activate', function () {
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+    mainWindow.on('close', function(e) {
+        e.preventDefault();
+        dialog.showMessageBox({
+            type: 'info',
+            title: '提示',
+            message:'确认退出？',
+            buttons: ['确认', '取消'],
+            cancelId: 1,
+        }).then(idx => {    
+            console.log(idx)
+            if (idx.response == 1) {
+                e.preventDefault();
+            } else {
+                mainWindow = null
+                app.exit();
+            }
+        })
     })
 })
 
 app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit()
+    // if (process.platform !== 'darwin') app.quit()
+    app.quit()
 })
