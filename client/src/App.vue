5c4521f4641d559ee2ea497808f87527a4be9b3e
Jimmy <jimmyrss1102@gmail.com>
2023-04-20 02:41:26 +0800

    refactor : use `electron-vite` create client

---
+++ client/src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  Cherry Noted
+</template>
+<script setup lang="ts">
+</script>
---
+++ client/src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import './samples/node-api';
+
+createApp(App)
+  .mount('#app')
+  .$nextTick(() => {
+    postMessage({ payload: 'removeLoading' }, '*');
+  });
