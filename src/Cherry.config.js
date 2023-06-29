447ef79c41656f112299ce8f49a7f380095eb727
AlanFuPC <alanfu@stu.pku.edu.cn>
2023-06-29 16:20:24 +0800

    update

--- src/Cherry.config.js
+++ src/Cherry.config.js
@@ -327,6 +327,7 @@ const defaultConfig = {
     { className: 'light', label: '明亮' },
     { className: 'green', label: '清新' },
     { className: 'red', label: '热情' },
+    { className: 'violet', label: '淡雅' },
   ],
   // 预览页面不需要绑定事件
   isPreviewOnly: false,
