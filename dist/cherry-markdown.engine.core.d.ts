cce4b1d523d0eb0e0e4fd409a3fe474e7bc2e66b
Ethan Xu <34962267+ufec@users.noreply.github.com>
2023-09-19 19:30:41 +0800

    Fix/mathml (#604)
    * Re-add files with Git LFS
    
    * fix: formula export mathml to word
    
    ---------
    
    Co-authored-by: ethanxxu <ethanxxu@tencent.com>

--- dist/cherry-markdown.engine.core.d.ts
+++ dist/cherry-markdown.engine.core.d.ts
@@ -1,3 +1,3 @@
-import CherryEngine, { MenuHookBase, SyntaxHookBase } from "./types/index.engine.core";
-export { MenuHookBase, SyntaxHookBase };
-export default CherryEngine;
+version https://git-lfs.github.com/spec/v1
+oid sha256:a8d8c68f52c6d728229fa6d2c0b846a7441821d7146598e47344a1abcdde6b95
+size 157
