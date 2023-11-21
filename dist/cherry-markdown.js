8a2d39978e2956fe76dda23c8a46cadbaad5da29
MarkGrass <abstract.voodoo@gmail.com>
2023-11-21 10:05:56 +0300

    fix package name

--- dist/cherry-markdown.js
+++ dist/cherry-markdown.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2a0b40dbfec150082eb14bb03b0e2feeab6d68730fd493e8e6a758d0c07a96a
-size 8604552
+oid sha256:982684faf0421c28945e9893e8735b7a8ddb255d5aa8a4c19ee4c7e4b06c9acd
+size 8332689
