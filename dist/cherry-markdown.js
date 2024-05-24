fb9d245d3740a538b16bdf49e50c450783b65bd1
Ethan Xu <34962267+ufec@users.noreply.github.com>
2024-05-24 14:15:39 +0800

    Fix: 右侧编辑列表过程中, 输入回车后, 再次编辑此列表, 数据会异常 #751 (#772)
    * chore(release): v0.8.36
    
    * fix: 右侧编辑列表过程中, 输入回车后, 再次编辑此列表, 数据会异常 #751
    
    * fix: remove console.log
    
    * fix: ignore event.target.innerText type error
    
    * fix: ignore event.target.innerText type error
    
    ---------
    
    Co-authored-by: jiawei686 <892001108@qq.com>
    Co-authored-by: humyfred <513407656@qq.com>

--- dist/cherry-markdown.js
+++ dist/cherry-markdown.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef1916dda9bd5085b2ce581a77d806472cd35a45feb31b39b063dcaa07958e09
+oid sha256:a0604eca9203eefb80cb9c6c45f9a4eaac8b3ee2decc368db0d8d39a5ae396f8
 size 7569177
