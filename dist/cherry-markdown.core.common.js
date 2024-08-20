f2e1addecec311aaf0cf6c2197dd01c1ff44ae7f
Evg <budo@narod.ru>
2024-08-20 05:59:28 +0300

    ADD: localization (Russian) (#875)
    * chore(release): 0.8.45
    
    * chore(release): 0.8.46
    
    * add: localization file (Russian)
    
    * Update index.js
    
    * Update index.js
    
    ---------
    
    Co-authored-by: sunsonliu(刘阳) <sunsonliu@tencent.com>

--- dist/cherry-markdown.core.common.js
+++ dist/cherry-markdown.core.common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80bfe3fad2fc623bf5055f3d803ae6d38a0530d6bda7fd446bbfe7c70e453197
-size 1300452
+oid sha256:0ac01c15fd8ab02d3faec627c3493c842412f2c802c6e4af93b2d24e14d073dc
+size 1411937
--- dist/cherry-markdown.esm.js
+++ dist/cherry-markdown.esm.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4f2cd286a4be66e46ef3cd2522abe9d576ae7bed0d99ea96337a9109f4d1290
-size 4058359
+oid sha256:b949495216a729b8bcb273b99c4ea8ddef8e7d4a1dd7f01c42506322ca847947
+size 4182325
