0f564e0188817d6cd3f9aaa9c04c52a8eaa254b1
阿菜 Cai <jimmyrss1102@gmail.com>
2025-01-15 16:06:58 +0800

    refactor: `yarn` monorepo (#1025)
    * refactor: use `yarn` create monorepo
    
    * chore: pause workflows ci
    
    * fix: 修正lock文件 (#2)
    
    Co-authored-by: yuxuanxia <yuxuanxia@tencent.com>
    
    * 移除 dist 文件夹 和 vscodePlugin 的  `@types/mocha` deps
    
    * chore: gitignore `dist`
    
    * 移动 commitizen
    
    * chore: remove `example/react-demo` 修复 cherry-markdown 打包问题
    
    * chore: 移动公共依赖
    
    * chore: 移动提交代码检测依赖
    
    * 同步代码
    
    * chore: 修正版本号
    
    * chore: 修改 engines node 限制
    
    ---------
    
    Co-authored-by: Leo Xia <52569690+Saraph1nes@users.noreply.github.com>
    Co-authored-by: yuxuanxia <yuxuanxia@tencent.com>

--- dist/addons/advance/cherry-table-echarts-plugin.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:078cef0a708e287892a1966a2e7a3f78495a1fad5a7357d0eaec53f09431766e
-size 42914
--- dist/cherry-markdown.engine.core.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3237579123a0c1fe2cdb53effd4dd13f766ce96e57ff7e477f678d9a0bb981ce
-size 549073
