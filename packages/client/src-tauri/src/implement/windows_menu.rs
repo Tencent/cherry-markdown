b07f50e1d2161fe9824229b3cc42156fecc82054
阿菜 Cai <jimmyrss1102@gmail.com>
2025-01-23 04:33:14 +0800

    refactor(client): `window_menu` and `system_tray_menu` (#1031)
    chore: use yarn package tools

--- packages/client/src-tauri/src/implement/windows_menu.rs
+++ packages/client/src-tauri/src/implement/windows_menu.rs
@@ -1,79 +1,110 @@
-use crate::utils::base::{
-    get_current_lang, get_current_show_toolbar, set_current_lang, subscribe_observer,
-    NotifyMessage,
-};
 use crate::utils::i18n::Language;
-use std::sync::{Arc, Mutex};
 use tauri::{
-    menu::{CheckMenuItemBuilder, Menu, MenuBuilder, SubmenuBuilder},
-    App, AppHandle, Emitter, Wry,
+    menu::{CheckMenuItemBuilder, MenuBuilder, MenuItemBuilder, SubmenuBuilder},
+    App, Emitter,
 };
-
-struct State {
-    lang_str: String,
-    is_show_toolbar: bool,
-}
-
-fn create_window_menu(
-    handle: &AppHandle,
-    language: Arc<Mutex<Language>>,
-    state: State,
-) -> Result<Menu<Wry>, tauri::Error> {
-    let language = language.lock().unwrap();
-    let file_menu = SubmenuBuilder::new(handle, language.file.get_lang(&state.lang_str))
-        .text("new_file", language.new_file.get_lang(&state.lang_str))
-        .text("open_file", language.open_file.get_lang(&state.lang_str))
-        .text("save", language.save.get_lang(&state.lang_str))
-        .text("save_as", language.save_as.get_lang(&state.lang_str))
-        .text("quit", language.quit.get_lang(&state.lang_str));
-
-    let language_sub_en = CheckMenuItemBuilder::new("English")
-        .id("en")
-        .checked(&state.lang_str == "en");
-
-    let language_sub_zh = CheckMenuItemBuilder::new("中文")
-        .id("zh")
-        .checked(&state.lang_str == "zh");
-
-    let language_menu = SubmenuBuilder::new(handle, language.language.get_lang(&state.lang_str))
-        .item(&language_sub_en.build(handle)?)
-        .item(&language_sub_zh.build(handle)?);
-
-    let toggle_toolbar = CheckMenuItemBuilder::new(language.show_toolbar.get_lang(&state.lang_str))
-        .id("toggle_toolbar")
-        .checked(state.is_show_toolbar);
-    let setting_menu = SubmenuBuilder::new(handle, language.setting.get_lang(&state.lang_str))
-        .item(&toggle_toolbar.build(handle)?);
-
-    let menu = MenuBuilder::new(handle)
-        .item(&file_menu.build()?)
-        .item(&language_menu.build()?)
-        .item(&setting_menu.build()?)
-        .build()?;
-    Ok(menu)
-}
 
 /// 窗口菜单
 pub fn window_menu(app: &mut App) -> Result<(), tauri::Error> {
     let handle = app.handle();
-    let language = Arc::new(Mutex::new(Language::new()));
+    let language = Language::new();
+    let lang_str = "en".to_string();
 
-    let menu = create_window_menu(
-        &handle,
-        language.clone(),
-        State {
-            lang_str: get_current_lang(),
-            is_show_toolbar: get_current_show_toolbar(),
-        },
-    )?;
+    let new_file_menu = MenuItemBuilder::with_id("new_file", language.new_file.get_lang(&lang_str))
+        .build(handle)?;
+    let open_file_menu =
+        MenuItemBuilder::with_id("open_file", language.open_file.get_lang(&lang_str))
+            .build(handle)?;
+    let save_menu =
+        MenuItemBuilder::with_id("save", language.save.get_lang(&lang_str)).build(handle)?;
+    let save_as_menu =
+        MenuItemBuilder::with_id("save_as", language.save_as.get_lang(&lang_str)).build(handle)?;
+    let quit_menu =
+        MenuItemBuilder::with_id("quit", language.quit.get_lang(&lang_str)).build(handle)?;
+
+    let file_menu = SubmenuBuilder::with_id(handle, "file", language.file.get_lang(&lang_str))
+        .items(&[
+            &new_file_menu,
+            &open_file_menu,
+            &save_menu,
+            &save_as_menu,
+            &quit_menu,
+        ])
+        .build()?;
+
+    let language_sub_en = CheckMenuItemBuilder::with_id("en", "English")
+        .checked(&lang_str == "en")
+        .build(handle)?;
+
+    let language_sub_zh = CheckMenuItemBuilder::with_id("zh", "中文")
+        .id("zh")
+        .checked(&lang_str == "zh")
+        .build(handle)?;
+
+    let language_menu = SubmenuBuilder::with_id(
+        handle,
+        "language_menu",
+        language.language.get_lang(&lang_str),
+    )
+    .items(&[&language_sub_en, &language_sub_zh])
+    .build()?;
+
+    let toggle_toolbar =
+        CheckMenuItemBuilder::with_id("toggle_toolbar", language.show_toolbar.get_lang(&lang_str))
+            .id("toggle_toolbar")
+            .checked(false)
+            .build(handle)?;
+
+    let setting_menu =
+        SubmenuBuilder::with_id(handle, "setting_menu", language.setting.get_lang(&lang_str))
+            .item(&toggle_toolbar)
+            .build()?;
+
+    let menu = MenuBuilder::new(handle)
+        .items(&[&file_menu, &language_menu, &setting_menu])
+        .build()?;
+
     app.set_menu(menu)?;
-    let handle_clone = handle.clone();
-    let language_clone = language.clone();
 
     app.on_menu_event(
         move |app_handle: &tauri::AppHandle, event| match event.id().0.as_str() {
             "en" | "zh" => {
-                set_current_lang(&event.id().0.as_str());
+                let lang_str = event.id().0.as_str();
+                file_menu
+                    .set_text(language.new_file.get_lang(&lang_str))
+                    .expect("set file_menu text failed");
+                new_file_menu
+                    .set_text(language.new_file.get_lang(&lang_str))
+                    .expect("set new_file_menu text failed");
+                open_file_menu
+                    .set_text(language.open_file.get_lang(&lang_str))
+                    .expect("set open_file_menu text failed");
+                save_menu
+                    .set_text(language.save.get_lang(&lang_str))
+                    .expect("set save_menu text failed");
+                save_as_menu
+                    .set_text(language.save_as.get_lang(&lang_str))
+                    .expect("set save_as_menu text failed");
+                quit_menu
+                    .set_text(language.quit.get_lang(&lang_str))
+                    .expect("set quit_menu text failed");
+
+                language_menu
+                    .set_text(language.language.get_lang(&lang_str))
+                    .expect("set language_menu text failed");
+                language_sub_en
+                    .set_checked(lang_str == "en")
+                    .expect("set en checked failed");
+                language_sub_zh
+                    .set_checked(lang_str == "zh")
+                    .expect("set zh checked failed");
+
+                toggle_toolbar
+                    .set_text(language.show_toolbar.get_lang(&lang_str))
+                    .expect("set toggle_toolbar text failed");
+                setting_menu
+                    .set_text(language.setting.get_lang(&lang_str))
+                    .expect("set setting_menu text failed");
             }
             "new_file" => {
                 let _ = app_handle.emit("new_file", "");
@@ -88,16 +119,6 @@ pub fn window_menu(app: &mut App) -> Result<(), tauri::Error> {
                 let _ = app_handle.emit("save_as", "");
             }
             "toggle_toolbar" => {
-                if let Ok(menu) = create_window_menu(
-                    &handle_clone,
-                    language_clone.clone(),
-                    State {
-                        lang_str: get_current_lang(),
-                        is_show_toolbar: get_current_show_toolbar(),
-                    },
-                ) {
-                    let _ = app_handle.set_menu(menu);
-                }
                 let _ = app_handle.emit("toggle_toolbar", "");
             }
             "quit" => {
@@ -105,43 +126,6 @@ pub fn window_menu(app: &mut App) -> Result<(), tauri::Error> {
             }
             _ => {}
         },
-    );
-
-    subscribe_observer(
-        "windows_menu".to_string(),
-        Box::new({
-            let handle_clone = handle.clone();
-            let language_clone = language.clone();
-
-            move |msg: NotifyMessage| {
-                match msg {
-                    NotifyMessage::CurrentLang(lang) => {
-                        if let Ok(menu) = create_window_menu(
-                            &handle_clone,
-                            language_clone.clone(),
-                            State {
-                                lang_str: lang,
-                                is_show_toolbar: get_current_show_toolbar(),
-                            },
-                        ) {
-                            let _ = handle_clone.set_menu(menu);
-                        }
-                    }
-                    NotifyMessage::IsShowToolbar(show) => {
-                        if let Ok(menu) = create_window_menu(
-                            &handle_clone,
-                            language_clone.clone(),
-                            State {
-                                lang_str: get_current_lang(),
-                                is_show_toolbar: show,
-                            },
-                        ) {
-                            let _ = handle_clone.set_menu(menu);
-                        }
-                    }
-                }
-            }
-        }) as Box<dyn Fn(NotifyMessage) + Send + Sync>,
     );
     Ok(())
 }
