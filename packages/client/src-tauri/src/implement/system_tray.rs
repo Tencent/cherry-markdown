982bfcd0574b81bf141c38e93b2ecc549ce7b12d
阿 Cai <jimmyrss1102@gmail.com>
2025-01-22 19:05:13 +0800

    refactor(client): `window_menu` and `system_tray_menu`
    chore: use yarn package tools

--- packages/client/src-tauri/src/implement/system_tray.rs
+++ packages/client/src-tauri/src/implement/system_tray.rs
@@ -1,25 +1,17 @@
 use tauri::{
     menu::{Menu, MenuItem},
-    tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
-    App, AppHandle, Wry,
+    tray::TrayIconBuilder,
+    App,
 };
 
 use crate::utils::i18n::Language;
 
-use crate::utils::base::{
-    get_current_lang, restore_and_focus_window, set_current_lang
-};
+use crate::utils::base::restore_and_focus_window;
 
-fn create_tray_menu(app: &AppHandle, lang_str: &str) -> Result<Menu<Wry>, tauri::Error> {
+pub fn system_tray_menu(app: &mut App) -> Result<(), tauri::Error> {
+    let lang_str = "en".to_string();
     let language = Language::new();
 
-    let show_main_window = MenuItem::with_id(
-        app,
-        "open_cherry_markdown",
-        language.open_cherry_markdown.get_lang(&lang_str),
-        true,
-        None::<&str>,
-    )?;
     let quit = MenuItem::with_id(
         app,
         "quit",
@@ -28,72 +20,27 @@ fn create_tray_menu(app: &AppHandle, lang_str: &str) -> Result<Menu<Wry>, tauri:
         None::<&str>,
     )?;
 
-    let language = MenuItem::with_id(
-        app,
-        "language",
-        language.language.get_lang(&lang_str),
-        true,
-        None::<&str>,
-    )?;
+    // todo
+    // let language = MenuItem::with_id(
+    //     app,
+    //     "language",
+    //     language.language.get_lang(&lang_str),
+    //     true,
+    //     None::<&str>,
+    // )?;
 
-    let menu = Menu::with_items(app, &[&show_main_window, &language, &quit])?;
-
-    Ok(menu)
-}
-
-pub fn system_tray_menu(app: &mut App) -> Result<(), tauri::Error> {
-    let app_handle_clone = app.handle().clone();
-
-    let system_menu = create_tray_menu(&app_handle_clone, &get_current_lang())?;
+    let system_menu = Menu::with_items(app, &[&quit])?;
 
     let system_tray = TrayIconBuilder::with_id("tray")
         .menu(&system_menu)
-        .menu_on_left_click(false)
+        .show_menu_on_left_click(false)
         .icon(app.default_window_icon().unwrap().clone())
         .build(app)?;
 
-    system_tray.on_tray_icon_event(|tray, event| match event {
-        TrayIconEvent::Click {
-            button: MouseButton::Left,
-            button_state: MouseButtonState::Up,
-            ..
-        } => {
-            let app = tray.app_handle();
-            restore_and_focus_window(app, "main");
-        }
-        _ => {}
-    });
-
-    let system_tray_clone = system_tray.clone();
-
     system_tray.on_menu_event(move |app_handle, event| match event.id.as_ref() {
         "show_main_window" => {
+            println!("show_main_window");
             restore_and_focus_window(app_handle, "main");
-        }
-        "language" => {
-            // 先读取当前的lang
-            let lang_str = get_current_lang();
-            // 如果是中文，就切换成英文
-            if lang_str == "zh" {
-                set_current_lang("en");
-            }
-            if lang_str == "en" {
-                set_current_lang("zh");
-            }
-            let lang_str_new = get_current_lang();
-
-            let new_menu = {
-                match create_tray_menu(app_handle, &lang_str_new) {
-                    Ok(menu) => menu,
-                    Err(e) => {
-                        eprintln!("Error creating tray menu: {:?}", e);
-                        return;
-                    }
-                }
-            };
-            if let Err(e) = system_tray_clone.set_menu(Some(new_menu.clone())) {
-                eprintln!("Error setting menu: {:?}", e);
-            }
         }
         "quit" => {
             app_handle.exit(0);
