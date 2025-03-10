d3d674ca7016f38a248568b92d0b4b0f2218b795
Saraph1nes <xiayx.leo@gmail.com>
2025-03-10 16:32:47 +0800

    feat: #1073 将demo依赖资源迁移到assets中
    feat: #1073 将drawio依赖迁移到assets

--- examples/assets/scripts/chatgpt-demo.js
+++ examples/assets/scripts/chatgpt-demo.js
@@ -245,7 +245,7 @@ var basicConfig = {
   },
 };
 
-fetch('./markdown/basic.md')
+fetch('./assets/markdown/basic.md')
   .then((response) => response.text())
   .then((value) => {
     var config = Object.assign({}, basicConfig, { value: value });
