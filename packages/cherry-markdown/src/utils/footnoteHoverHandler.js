d7c90f780aa74c4b0f8ca453949ab3487401d4b5
sunsonliu(刘阳) <sunsonliu@tencent.com>
2025-05-15 16:51:46 +0800

    fix: #1188 修复脚注悬浮展示报错的问题

--- packages/cherry-markdown/src/utils/footnoteHoverHandler.js
+++ packages/cherry-markdown/src/utils/footnoteHoverHandler.js
@@ -74,7 +74,8 @@ export default class FootnoteHoverHandler {
     this.bubbleCard.refTitle = this.target.getAttribute('data-key');
     this.bubbleCard.content =
       this.previewerDom.querySelector(`.one-footnote[data-index="${this.bubbleCard.refNum}"]`).innerHTML ?? '';
-    const themeClass = this.previewerDom.className.match(/theme__[^\s]+/)[0] ?? '';
+    const previewClassName = this.previewerDom.className ?? '';
+    const themeClass = /theme__[^\s]+/.test(previewClassName) ? previewClassName.match(/theme__[^\s]+/)[0] ?? '' : '';
     this.container.className = `${this.container.className.replace(/(^|\s)theme__[^\s]+/g, '')} ${themeClass}`;
     const customContent =
       this.bubbleConfig?.render(
