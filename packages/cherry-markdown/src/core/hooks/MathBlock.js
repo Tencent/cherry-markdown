dec293f792a41c3369ca261b9990b8654751ca47
Jiadezhende <3139966468@qq.com>
2025-07-19 20:04:52 +0800

    fix: 简化样式设计，复用css变量；同时优化代码块展开键样式，使其更易点击

--- packages/cherry-markdown/src/core/hooks/MathBlock.js
+++ packages/cherry-markdown/src/core/hooks/MathBlock.js
@@ -34,7 +34,7 @@ export default class MathBlock extends ParagraphBase {
   constructor({ config }) {
     super({ needCache: true });
     // 非浏览器环境下配置为 node
-    this.engine = isBrowser() ? (config.engine ?? 'MathJax') : 'node';
+    this.engine = isBrowser() ? config.engine ?? 'MathJax' : 'node';
   }
 
   toHtml(wholeMatch, lineSpace, leadingChar, content) {
