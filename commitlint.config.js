f6ab2688822e6efa3bfefc871b93fc1ecccfc50b
RSS1102 <jimmyrss1102@gmail.com>
2025-08-18 15:25:55 +0800

    chore(eslint): `no-unused-vars` is error, and fix related errors
    chore: remove scope-enum commit restriction

--- commitlint.config.js
+++ commitlint.config.js
@@ -21,17 +21,17 @@ module.exports = {
         // 待补充
       ],
     ],
-    'scope-enum': [
-      2,
-      'always',
-      [
-        'core', // cherry-markdown 项目
-        'client', // 客户端部分
-        'vscodePlugin', // VSCode 插件
-        'global', // Monorepo 结构(可省略)
-        'scripts', // 脚本
-        // 待补充
-      ],
-    ],
+    // 'scope-enum': [
+    //   2,
+    //   'always',
+    //   [
+    //     'core', // cherry-markdown 项目
+    //     'client', // 客户端部分
+    //     'vscodePlugin', // VSCode 插件
+    //     'global', // Monorepo 结构(可省略)
+    //     'scripts', // 脚本
+    //     // 待补充
+    //   ],
+    // ],
   },
 };
