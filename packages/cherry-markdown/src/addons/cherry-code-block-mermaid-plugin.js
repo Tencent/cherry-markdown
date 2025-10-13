0f6fd964ed446e354e003c347e66db3d2aef831b
阿菜 Cai <jimmyrss1102@gmail.com>
2025-10-13 14:28:38 +0800

    fix(mermaid): 修复 mermaid 在 shadow dom 中无法选中元素的问题 (#1460)
    * fix(mermaid): 修复 mermaid 在 shadow dom 中无法选中元素的问题
    
    * add changeset log

--- packages/cherry-markdown/src/addons/cherry-code-block-mermaid-plugin.js
+++ packages/cherry-markdown/src/addons/cherry-code-block-mermaid-plugin.js
@@ -78,6 +78,39 @@ export default class MermaidCodeEngine {
   dom = null;
   mermaidCanvas = null;
 
+  /**
+   * @param {Object} mermaidOptions - Mermaid 配置选项
+   * @param {Object} [mermaidOptions.mermaid] - mermaid 实例对象，如果未提供会尝试从 window.mermaid 获取
+   * @param {Object} [mermaidOptions.mermaidAPI] - mermaidAPI 实例对象，如果未提供会尝试从 window.mermaidAPI 获取
+   * @param {string} [mermaidOptions.theme='default'] - 主题，可选值: 'default', 'dark', 'forest', 'neutral' 等
+   * @param {string} [mermaidOptions.altFontFamily='sans-serif'] - 备用字体
+   * @param {string} [mermaidOptions.fontFamily='sans-serif'] - 主字体
+   * @param {string} [mermaidOptions.themeCSS] - 自定义主题 CSS 样式
+   * @param {boolean} [mermaidOptions.startOnLoad=false] - 是否在页面加载时自动渲染
+   * @param {number} [mermaidOptions.logLevel=5] - 日志级别，1-5，5 为最详细
+   * @param {HTMLElement} [mermaidOptions.mermaidCanvasAppendDom] - Mermaid 临时画布容器的挂载节点
+   * @param {Object} [mermaidOptions.flowchart] - 流程图配置，可设置 { useMaxWidth: false } 等
+   * @param {Object} [mermaidOptions.sequence] - 序列图配置，可设置 { useMaxWidth: false } 等
+   * @param {Object} [mermaidOptions.gantt] - 甘特图配置，可设置 { useMaxWidth: false } 等
+   * @param {Object} [mermaidOptions.journey] - 用户旅程图配置，可设置 { useMaxWidth: false } 等
+   * @param {Object} [mermaidOptions.timeline] - 时间线图配置，可设置 { useMaxWidth: false } 等
+   * @param {Object} [mermaidOptions.class] - 类图配置，可设置 { useMaxWidth: false } 等
+   * @param {Object} [mermaidOptions.state] - 状态图配置，可设置 { useMaxWidth: false } 等
+   * @param {Object} [mermaidOptions.er] - ER 图配置，可设置 { useMaxWidth: false } 等
+   * @param {Object} [mermaidOptions.pie] - 饼图配置，可设置 { useMaxWidth: false } 等
+   * @param {Object} [mermaidOptions.quadrantChart] - 象限图配置，可设置 { useMaxWidth: false } 等
+   * @param {Object} [mermaidOptions.xyChart] - XY 图配置，可设置 { useMaxWidth: false } 等
+   * @param {Object} [mermaidOptions.requirement] - 需求图配置，可设置 { useMaxWidth: false } 等
+   * @param {Object} [mermaidOptions.architecture] - 架构图配置，可设置 { useMaxWidth: false } 等
+   * @param {Object} [mermaidOptions.mindmap] - 思维导图配置，可设置 { useMaxWidth: false } 等
+   * @param {Object} [mermaidOptions.kanban] - 看板图配置，可设置 { useMaxWidth: false } 等
+   * @param {Object} [mermaidOptions.gitGraph] - Git 图配置，可设置 { useMaxWidth: false } 等
+   * @param {Object} [mermaidOptions.c4] - C4 图配置，可设置 { useMaxWidth: false } 等
+   * @param {Object} [mermaidOptions.sankey] - 桑基图配置，可设置 { useMaxWidth: false } 等
+   * @param {Object} [mermaidOptions.packet] - 数据包图配置，可设置 { useMaxWidth: false } 等
+   * @param {Object} [mermaidOptions.block] - 块图配置，可设置 { useMaxWidth: false } 等
+   * @param {Object} [mermaidOptions.radar] - 雷达图配置，可设置 { useMaxWidth: false } 等
+   */
   constructor(mermaidOptions = {}) {
     const { mermaid, mermaidAPI } = mermaidOptions;
     if (
@@ -110,7 +143,7 @@ export default class MermaidCodeEngine {
     }
     this.mermaidCanvas = document.createElement('div');
     this.mermaidCanvas.style = 'width:1024px;opacity:0;position:fixed;top:100%;';
-    const container = $engine.$cherry.wrapperDom || document.body;
+    const container = this.options.mermaidCanvasAppendDom || $engine.$cherry.wrapperDom || document.body;
     container.appendChild(this.mermaidCanvas);
   }
 
