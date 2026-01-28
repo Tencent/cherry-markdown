8261c5bbccfced426f40d6987b578f60ec8dd54d
novlan1 <1576271227@qq.com>
2026-01-28 17:27:40 +0800

    feat: add stream package

---
+++ packages/cherry-markdown/build/rollup.stream.config.js
@@ -0,0 +1,87 @@
+/**
+ * Copyright (C) 2021 Tencent.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import terser from '@rollup/plugin-terser';
+import baseConfig from './rollup.base.config.js';
+
+const terserPlugin = (options = {}) =>
+  terser({
+    output: {
+      comments: false,
+    },
+    compress: {
+      pure_funcs: ['console.log', 'console.info'],
+    },
+    ecma: 5,
+    ...options,
+  });
+
+const umdOutputConfig = {
+  ...baseConfig.output,
+  exports: 'named',
+  file: 'dist/cherry-markdown.stream.js',
+  format: 'umd',
+  name: 'Cherry',
+  sourcemap: false,
+  compact: true,
+  plugins: [terserPlugin()],
+  globals: {
+    mermaid: 'mermaid',
+    codemirror: 'CodeMirror',
+    'codemirror/src/util/misc': 'CodeMirror',
+  },
+};
+
+const esmOutputConfig = {
+  ...baseConfig.output,
+  file: 'dist/cherry-markdown.stream.esm.js',
+  format: 'esm',
+  name: 'Cherry',
+  sourcemap: false,
+  compact: true,
+  plugins: [
+    terserPlugin({
+      module: true,
+      ecma: 2015,
+    }),
+  ],
+};
+
+const options = {
+  ...baseConfig,
+  input: 'src/index.stream.core.js',
+  output: [umdOutputConfig, esmOutputConfig],
+};
+
+if (!Array.isArray(options.external)) {
+  options.external = [];
+}
+// 流式渲染包不需要mermaid和codemirror
+options.external.push('mermaid');
+options.external.push('codemirror');
+options.external.push(/^codemirror\/.*/); // 排除所有codemirror子模块
+
+/** 构建目标是否 node */
+const IS_COMMONJS_BUILD = process.env.BUILD_TARGET === 'commonjs';
+
+if (IS_COMMONJS_BUILD) {
+  options.output = {
+    ...umdOutputConfig,
+    file: umdOutputConfig.file.replace(/\.js$/, '.common.js'),
+    format: 'cjs',
+  };
+}
+
+export default options;
